874c4cd96c1927313e5a7eaf6a54b09a2d634eb6
SColson82 <sharon.colson@gmail.com>
2024-06-14 12:20:53 -0500

    change to formatting and verbiage

--- sdxlib/__init__.py
+++ sdxlib/__init__.py
@@ -5,7 +5,4 @@ A Python client library for interacting with the AtlanticWave-SDX L2VPN API.
 
 Run from the parent SDXLIB directory using:
     python -m unittest discover -v tests
-
-Created by: Sharon Colson
-Last Modified: 06/12/2024
 """
--- tests/__init__.py
+++ tests/__init__.py
@@ -3,7 +3,4 @@ Unit tests for the sdxlib library.
 
 Run from the parent SDXLIB directory using:
     python -m unittest discover -v tests
-
-Created by: Sharon Colson
-Last Modified: 06/12/2024
 """
