be7e990c81d51016774ef9a02ee16c57df8a4f0e
SColson82 <sharon.colson@gmail.com>
2024-06-14 16:41:14 -0500

    add unit tests to check that endpoints are list of at least 2 dicts

--- tests/test_client.py
+++ tests/test_client.py
@@ -12,6 +12,7 @@ Run from the SDXLIB parent directory Using:
 
 class TestSDXClient(unittest.TestCase):
 
+    #### API Call Succeeds ####
     # @patch ensures that the tests do not actually perform HTTP requests but instead test the behavior of the 'SDXClient'
     @patch('sdxlib.client.requests.post')
     def test_create_l2vpn_success(self, mock_post):
@@ -21,29 +22,24 @@ class TestSDXClient(unittest.TestCase):
 
         client = SDXClient(base_url="http://example.com")
         client.name = "Test L2VPN"
-        client.endpoints=[{"port_id": "urn:sdx:port:test:1", "vlan": "100"}]
+        client.endpoints=[
+            {"port_id": "urn:sdx:port:test:1", "vlan": "100"},
+            {"port_id": "urn:sdx:port:test:1", "vlan": "200"}
+        ]
         
         response = client.create_l2vpn()
 
         self.assertEqual(response, {"status": "Accepted"})
 
-    @patch('sdxlib.client.requests.post')
-    def test_create_l2vpn_name_length_exceeds_limit(self, mock_post):
-        """Checks that the 'create_l2vpn' method correctly raises a 'ValueError' when the 'name' exceeds 50 characters. """
-        client = SDXClient(base_url="http://example.com")
-        client.name="This is a very long name that exceeds 50 chatacters limit"
-        client.endpoints=[{"port_id":"urn:sdx:port:test:1", "vlan": "100"}]
-
-        with self.assertRaises(ValueError) as context:
-            client.create_l2vpn()
-
-        self.assertEqual(str(context.exception), "Name must be 50 characters or fewer.")
-
+    #### Name Attribute ####
     @patch('sdxlib.client.requests.post')
     def test_create_l2vpn_name_required(self, mock_post):
         """Checks that the 'create_l2vpn' method correctly raises a 'ValueError' when the 'name' is not provided (empty string)."""
         client = SDXClient(base_url="http://example.com")
-        client.endpoints=[{"port_id": "urn:sdx:port:test:1", "vlan": "100"}]
+        client.endpoints=[
+            {"port_id": "urn:sdx:port:test:1", "vlan": "100"},
+            {"port_id": "urn:sdx:port:test:1", "vlan": "200"}
+        ]
 
         with self.assertRaises(ValueError) as context:
             client.create_l2vpn()
@@ -51,8 +47,24 @@ class TestSDXClient(unittest.TestCase):
         self.assertEqual(str(context.exception), "Name is required.")
 
     @patch('sdxlib.client.requests.post')
+    def test_create_l2vpn_name_length_exceeds_limit(self, mock_post):
+        """Checks that the 'create_l2vpn' method correctly raises a 'ValueError' when the 'name' exceeds 50 characters. """
+        client = SDXClient(base_url="http://example.com")
+        client.name="This is a very long name that exceeds 50 chatacters limit"
+        client.endpoints=[
+            {"port_id": "urn:sdx:port:test:1", "vlan": "100"},
+            {"port_id": "urn:sdx:port:test:1", "vlan": "200"}
+        ]
+
+        with self.assertRaises(ValueError) as context:
+            client.create_l2vpn()
+
+        self.assertEqual(str(context.exception), "Name must be 50 characters or fewer.")
+
+    #### Endpoint Attribute ####
+    @patch('sdxlib.client.requests.post')
     def test_create_l2vpn_endpoints_required(self, mock_post):
-        """Checks that the endpoints list is not empty."""
+        """Checks that the 'create_l2vpn' method correctly raises a 'ValueError' when the endpoints list is empty."""
         client = SDXClient(base_url="http://example.com")
         client.name = "Test L2VPN"
 
@@ -62,6 +74,47 @@ class TestSDXClient(unittest.TestCase):
         self.assertEqual(str(context.exception), "Endpoints must not be empty.")
 
     @patch('sdxlib.client.requests.post')
+    def test_create_l2vpn_endpoints_min_required(self, mock_post):
+        """Checks that the 'endpoints' setter correctly raises 'ValueError' when the 'endpoints' contain less than 2 entries."""
+        
+        client = SDXClient(base_url="http://example.com")
+        client.name="Test L2VPN"
+
+        with self.assertRaises(ValueError) as context:
+            client.endpoints=[
+                {"port_id":"urn:sdx:port:test:1", "vlan": "100"}
+            ]
+
+        self.assertEqual(str(context.exception), "Endpoints must contain at least 2 entries.")
+
+    @patch('sdxlib.client.requests.post')
+    def test_create_l2vpn_endpoints_type_check(self, mock_post):
+        """Checks that the 'endpoints' setter raises 'TypeError' when 'endpoints' is not a list."""
+        client = SDXClient(base_url="http://example.com")
+        client.name="Test L2VPN"
+
+
+        with self.assertRaises(TypeError) as context:
+            client.endpoints = "invalid endpoints"
+
+        self.assertEqual(str(context.exception), "Endpoints must be a list.")
+
+    @patch('sdxlib.client.requests.post')
+    def test_create_l2vpn_endpoints_list_of_dicts_check(self, mock_post):
+        """Checks that the 'endpoints' setter raises 'TypeError' when 'endpoints' contains non-dictionary elements."""
+        client = SDXClient(base_url="http://example.com")
+        client.name="Test L2VPN"
+
+        with self.assertRaises(TypeError) as context:
+            client.endpoints=[
+                {"port_id":"urn:sdx:port:test:1", "vlan": "100"},
+                "invalid endpoint"
+            ]
+
+        self.assertEqual(str(context.exception), "Endpoints must be a list of dictionaries.")
+
+    #### API Call Fails ####
+    @patch('sdxlib.client.requests.post')
     def test_create_l2vpn_failure(self, mock_post):
         """Checks that the 'create_l2vpn' method correctly handles a failure scenario where the API call returns an error response."""
         mock_post.return_value.ok = False
@@ -70,7 +123,10 @@ class TestSDXClient(unittest.TestCase):
 
         client = SDXClient(base_url="http://example.com")
         client.name="Test L2VPN"
-        client.endpoints=[{"port_id": "urn:sdx:port:test:1", "vlan": "100"}]
+        client.endpoints=[
+                {"port_id": "urn:sdx:port:test:1", "vlan": "100"},
+                {"port_id": "urn:sdx:port:test:1", "vlan": "200"}
+        ]
 
         with self.assertRaises(SDXException) as context:
             client.create_l2vpn()
