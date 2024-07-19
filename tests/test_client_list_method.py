0bf839b0c768c9268ea70440d0f85851bfcfb7a2
SColson82 <sharon.colson@gmail.com>
2024-07-19 14:12:36 -0500

    add separate file for list method unit tests

---
+++ tests/test_client_list_method.py
@@ -0,0 +1,10 @@
+import requests
+import unittest
+from unittest.mock import patch
+from sdxlib.sdx_client import SDXClient, SDXException
+
+class TestSDXClient(unittest.TestCase):
+
+# Run the tests
+if __name__ == "__main__":
+    unittest.main()
