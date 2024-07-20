e68f53ecf7c8d054f7c4e7eb3acd673d3d6a02e4
SColson82 <sharon.colson@gmail.com>
2024-07-20 11:36:33 -0500

    add config file for unit test variables

---
+++ tests/test_config.py
@@ -0,0 +1,12 @@
+TEST_URL = "http://example.com"
+TEST_NAME = "Test L2VPN"
+TEST_ENDPOINTS = [
+    {
+        "port_id": "urn:sdx:port:test-oxp_url:test-node_name:test-port_name",
+        "vlan": "100",
+    },
+    {
+        "port_id": "urn:sdx:port:test-oxp_url:test-node_name:test-port_name2",
+        "vlan": "200",
+    },
+]
