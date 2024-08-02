8853164bfcb9c8e2c654da49b28a26f0814b8774
SColson82 <sharon.colson@gmail.com>
2024-08-02 14:32:06 -0500

    add variables for error messages and a client setup method

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,5 @@
+from sdxlib.sdx_client import SDXClient
+
 TEST_URL = "http://example.com"
 TEST_NAME = "Test L2VPN"
 TEST_ENDPOINTS = [
@@ -10,3 +12,13 @@ TEST_ENDPOINTS = [
         "vlan": "200",
     },
 ]
+
+ERROR_NAME_INVALID= "Name must be a non-empty string with maximum 50 characters."
+ERROR_ENDPOINTS_LIST = "Endpoints must be a list."
+ERROR_ENDPOINTS_MIN_ENTRIES = "Endpoints must contain at least 2 entries."
+ERROR_ENDPOINTS_LIST_DICTS = "Endpoints must be a list of dictionaries."
+ERROR_ENDPOINTS_VLAN_SAME = "All endpoints must have the same VLAN value is one endpoint is 'all' or a range."
+ERROR_ENDPOINTS_INVALID_VLAN = "Invalid VLAN value: '{}'. Must be 'any', 'all', 'untagged', a string representing an integer between 1 and 4095, or a range."
+
+def create_client(base_url=TEST_URL, name=TEST_NAME, endpoints=TEST_ENDPOINTS):
+    return SDXClient(base_url=TEST_URL, name=TEST_NAME, endpoints=TEST_ENDPOINTS)
