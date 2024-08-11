12e59075d47b7edd4da2eb71ccd2fde8718c2899
SColson82 <sharon.colson@gmail.com>
2024-08-11 15:13:32 -0500

    add config file to hold often used variables

--- tests/test_config.py
+++ tests/test_config.py
@@ -12,13 +12,50 @@ TEST_ENDPOINTS = [
         "vlan": "200",
     },
 ]
+VLAN_100 = {
+    "port_id": "urn:sdx:port:test-oxp_url:test-node_name:test-port_name",
+    "vlan": "100",
+}
+VLAN_200 = {
+    "port_id": "urn:sdx:port:test-oxp_url:test-node_name:test-port_name2",
+    "vlan": "200",
+}
+VLAN_ANY = {
+    "port_id": "urn:sdx:port:test-oxp_url:test-node_name:test-port_name",
+    "vlan": "any",
+}
+VLAN_ALL = {
+    "port_id": "urn:sdx:port:test-ox_url:test-node_name:test-port_name",
+    "vlan": "all",
+}
+VLAN_RANGE = {
+    "port_id": "urn:sdx:port:test-oxp_url:test-node_name:test-port_name",
+    "vlan": "100:200",
+}
+VLAN_UNTAGGED = {
+    "port_id": "urn:sdx:port:test-oxp_url:test-node_name:test-port_name2",
+    "vlan": "untagged",
+}
 
-ERROR_NAME_INVALID= "Name must be a non-empty string with maximum 50 characters."
-ERROR_ENDPOINTS_LIST = "Endpoints must be a list."
-ERROR_ENDPOINTS_MIN_ENTRIES = "Endpoints must contain at least 2 entries."
-ERROR_ENDPOINTS_LIST_DICTS = "Endpoints must be a list of dictionaries."
-ERROR_ENDPOINTS_VLAN_SAME = "All endpoints must have the same VLAN value is one endpoint is 'all' or a range."
-ERROR_ENDPOINTS_INVALID_VLAN = "Invalid VLAN value: '{}'. Must be 'any', 'all', 'untagged', a string representing an integer between 1 and 4095, or a range."
+# Name error message.
+ERROR_NAME_INVALID = "Name must be a non-empty string with maximum 50 characters."
+
+# Endpoint error messages.
+ERROR_INVALID_ENDPOINTS = "Endpoints must be a list."
+ERROR_EMPTY_ENDPOINTS_LIST = "Endpoints list cannot be empty."
+ERROR_MIN_ENTRIES = "Endpoints must contain at least 2 entries."
+ERROR_LIST_OF_DICTS = "Endpoints must be a list of dictionaries."
+ERROR_NONEMPTY_PORT_ID = "Each endpoint must contain a non-empty 'port_id' key."
+ERROR_INVALID_PORT_ID_FORMAT = "Invalid port_id format: invalid-port_id"
+ERROR_EMPTY_VLAN_VALUE = "Each endpoint must contain a non-empty 'vlan' key."
+ERROR_INVALID_VLAN_TYPE = "VLAN must be a string."
+ERROR_INVALID_VLAN_VALUE = "Invalid VLAN value: '{}'. Must be between 1 and 4095."
+ERROR_VLAN_RANGE_MISMATCH = (
+    "All endpoints must have the same VLAN value if one endpoint is 'all' or a range."
+)
+ERROR_VLAN_INVALID = "Invalid VLAN value: '{}'. Must be 'any', 'all', 'untagged', a string representing an integer between 1 and 4095, or a range."
+ERROR_VLAN_RANGE_VALUE = "Invalid VLAN range format: '{}'. Must be 'VLAN ID1:VLAN ID2'."
+
 
 def create_client(base_url=TEST_URL, name=TEST_NAME, endpoints=TEST_ENDPOINTS):
     return SDXClient(base_url=TEST_URL, name=TEST_NAME, endpoints=TEST_ENDPOINTS)
